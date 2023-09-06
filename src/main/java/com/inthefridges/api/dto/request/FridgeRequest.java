f95cc9203d4aaff5882912f6e97e1b9de5ce80a4
zerohyun.j <zerohyun.j@gmail.com>
2023-09-06 20:55:06 +0900

    [fix] FridgesController 호출 시 AnonymouseAutentication 객체 생성되는 문제
    * Controller uri 수정

--- src/main/java/com/inthefridges/api/dto/request/FridgeRequest.java
+++ src/main/java/com/inthefridges/api/dto/request/FridgeRequest.java
@@ -1,15 +1,11 @@
 package com.inthefridges.api.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record FridgeRequest(
         @NotBlank(message = "제목은 빈칸으로 둘 수 없습니다.")
-        @Max(value = 20, message = "제목은 한 글자 이상, 20자 이내로 작성해야 합니다.")
+        @Size(max = 20, message = "제목은 한 글자 이상, 20자 이내로 작성해야 합니다.")
         String name
-//        @NotBlank(message = "타입은 빈칸으로 둘 수 없습니다.")
-//        @PositiveOrZero(message = "타입은 0을 포함한 양수여야 합니다.")
-//        Long[] type
  ) {
 }
