1422ea7b01ad9ed275e54f30244791c446210914
zerohyun.j <zerohyun.j@gmail.com>
2023-09-23 20:12:51 +0900

    [refector] FridgeRequest Validation 메세지 변경

--- src/main/java/com/inthefridges/api/dto/request/FridgeRequest.java
+++ src/main/java/com/inthefridges/api/dto/request/FridgeRequest.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public record FridgeRequest(
-        @NotBlank(message = "제목은 빈칸으로 둘 수 없습니다.")
-        @Size(max = 20, message = "제목은 한 글자 이상, 20자 이내로 작성해야 합니다.")
+        @NotBlank(message = "이름은 빈칸으로 둘 수 없습니다.")
+        @Size(max = 20, message = "이름은 한 글자 이상, 20자 이내로 작성해야 합니다.")
         String name,
-        @PositiveOrZero(message = "아이디는 음수일 수 없습니다.")
+        @PositiveOrZero(message = "파일 아이디는 음수일 수 없습니다.")
         Long fileId
  ) {
 }
