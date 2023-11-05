4759cc40ccdf3a30340410b1fd6cd4d242833e89
zerohyun-j <138653094+zerohyun-j@users.noreply.github.com>
2023-11-05 20:35:41 +0900

    [refactor] 식품 생성,수정 Reqeust 객체 분리 및 ItemMapper 클래스 구현
    * DTO <-> Entity 변환 Mapper 클래스 구현

--- src/main/java/com/inthefridges/api/dto/request/CreateItemRequest.java
+++ src/main/java/com/inthefridges/api/dto/request/CreateItemRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.*;
 
 import java.util.Date;
 
-public record ItemRequest(
+public record CreateItemRequest (
         @NotBlank(message = "식품 이름은 빈칸으로 둘 수 없습니다.")
         @Size(max = 50, message = "식품 이름은 한 글자 이상, 50자 이내로 작성해야 합니다.")
         String name,
@@ -24,6 +24,6 @@ public record ItemRequest(
 
         @NotNull(message = "저장 타입 아이디는 빈칸으로 둘 수 없습니다.")
         @PositiveOrZero(message = "저장 타입 아이디는 음수일 수 없습니다.")
-        int storageTypeId
-) {
+        int storageTypeId){
+
 }
